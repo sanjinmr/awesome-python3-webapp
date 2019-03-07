0c58a85921b33c4567908f2c7527016f55fb9205
dongmingxin <dongmingxin@yinyueke.com>
2019-03-08 00:24:31 +0800

    构建前端

--- www/handlers.py
+++ www/handlers.py
@@ -7,12 +7,19 @@ __author__ = 'sanjinmr'
 
 from coroweb import get
 
-from models import User
+from models import User, Blog
 
 @get('/')
 async def index(request):
 	users = await User.findAll()
+	summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+	blogs = [
+		Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+		Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-3600),
+		Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-7200)
+	]
 	return {
-		'__template__': 'test.html',
-		'users': users
+		'__template__': 'blogs.html',
+		'users': users,
+		'blogs': blogs
 	}
