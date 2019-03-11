0e43715c3286e5b515baf04ff4d4b6edef88d41d
dongmingxin <dongmingxin@yinyueke.com>
2019-03-11 22:41:40 +0800

    修复created_at书写错误的问题

--- www/handlers.py
+++ www/handlers.py
@@ -9,14 +9,16 @@ from coroweb import get
 
 from models import User, Blog
 
+import time
+
 @get('/')
 async def index(request):
 	users = await User.findAll()
 	summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
 	blogs = [
-		Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
-		Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-3600),
-		Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-7200)
+		Blog(id='1', name='Test Blog 1', summary=summary, created_at=time.time()-120),
+		Blog(id='2', name='Test Blog 2', summary=summary, created_at=time.time()-3600),
+		Blog(id='3', name='Test Blog 3', summary=summary, created_at=time.time()-7200)
 	]
 	return {
 		'__template__': 'blogs.html',
