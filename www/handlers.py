e89bfa767443b0b79b2cf92b7552034fda02b4ee
dongmingxin <dongmingxin@yinyueke.com>
2019-03-13 09:07:28 +0800

    完成api_register_user

--- www/handlers.py
+++ www/handlers.py
@@ -7,11 +7,15 @@ __author__ = 'sanjinmr'
 
 from coroweb import get
 
-from models import User, Blog
+from models import User, Blog, next_id
 
 import time
 
 from apis import Page, APIValueError
+
+
+COOKIE_NAME = 'awesession'
+_COOKIE_KEY = configs.session.secret
 
 def get_page_index(page_str):
 	p = 1
@@ -23,7 +27,17 @@ def get_page_index(page_str):
 		p = 1
 	return p
 	
-
+def user2cookie(uesr, max_age):
+	'''
+	Generate cookie str by user.
+	'''
+	#build cookie string by: id-express-sha1
+	expires = str(int(time.time() + max_age))
+	s = '%s-%s-%s-%s' % (user.id, user.passwd, expires, _COOKIE_KEY)
+	L = [user.id, expires, hashlib.sha1(s.encode('utf-8')).hexdigest()]
+	return '-'.join(L)
+	
+	
 @get('/')
 async def index(request):
 	users = await User.findAll()
@@ -50,3 +64,32 @@ async def api_get_users(*, page = '1'):
 	for u in users:
 		u.passwd = '******'
 	return dict(page = p, users = users)
+	
+_RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
+_RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
+
+@POST('/api/users')
+def api_register_user(*, email, name, passwd):
+	if not name or not name.strip():
+		raise APIValueError('name')
+	if not email or not _RE_EMAIL.match(email):
+		raise APIValueError('email')
+	if not passwd or not _RE_SHA1.match(passwd):
+		raise APIValueError('passwd')
+	users = await User.findAll('email=?', [email])
+	if len(users) > 0:
+		raise APIError('register:failed', 'email', 'Email is already in use.')
+	uid = next_id()
+	sha1_passwd = '%s:%s' % (uid, passwd)
+	user = User(id=uid, name=name.strip(), email=email)
+	await user.save()
+	# make session cookie
+	r = web.Response()
+	r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
+	user.passwd = '******'
+	r.content_type = 'application/json'
+	r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
+	return r
+	
+	
+	
