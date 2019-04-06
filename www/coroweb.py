707d29d7fc801bb6cf6951bee018d1fa6455f2b7
dongmingxin <dongmingxin@yinyueke.com>
2019-04-06 20:27:56 +0800

    完成注册

--- www/coroweb.py
+++ www/coroweb.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'sanjin'
+__author__ = 'Michael Liao'
 
 import asyncio, os, inspect, logging, functools
 
@@ -11,237 +11,164 @@ from aiohttp import web
 
 from apis import APIError
 
-
 def get(path):
-	'''
-	Define decorator @get('/path')
-	'''
-	def decorator(func):
-		@functools.wraps(func)
-		def wrapper(*args, **kw):
-			return func(*args, **kw)
-		wrapper.__method__ = 'GET'
-		wrapper.__route__ = path
-		return wrapper
-	return decorator
-
+    '''
+    Define decorator @get('/path')
+    '''
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kw):
+            return func(*args, **kw)
+        wrapper.__method__ = 'GET'
+        wrapper.__route__ = path
+        return wrapper
+    return decorator
 
 def post(path):
-	'''
-	Define decorator @post('/path')
-	'''
-	def decorator(func):
-		@functools.wraps(func)
-		def wrapper(*args, **kw):
-			return func(*args, **kw)
-		wrapper.__method__ = 'POST'
-		wrapper.__route__ = path
-		return wrapper
-	return decorator
+    '''
+    Define decorator @post('/path')
+    '''
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kw):
+            return func(*args, **kw)
+        wrapper.__method__ = 'POST'
+        wrapper.__route__ = path
+        return wrapper
+    return decorator
 
-#获取必不可少的参数
-#即获取函数的不可变，且没有默认值的关键字参数
 def get_required_kw_args(fn):
-	args = []
-	params = inspect.signature(fn).parameters
-	for name, param in params.items():
-		#不可变且没有默认值
-		if param.kind == inspect.Parameter.KEYWORD_ONLY and param.default == inspect.Parameter.empty:
-			args.append(name)
-	return tuple(args)
+    args = []
+    params = inspect.signature(fn).parameters
+    for name, param in params.items():
+        if param.kind == inspect.Parameter.KEYWORD_ONLY and param.default == inspect.Parameter.empty:
+            args.append(name)
+    return tuple(args)
 
-#获取不可变关键字参数（无论是否有默认值）
 def get_named_kw_args(fn):
-	args = []
-	params = inspect.signature(fn).parameters
-	for name, param in params.items():
-		if param.kind == inspect.Parameter.KEYWORD_ONLY:
-			args.append(name)
-	return tuple(args)
+    args = []
+    params = inspect.signature(fn).parameters
+    for name, param in params.items():
+        if param.kind == inspect.Parameter.KEYWORD_ONLY:
+            args.append(name)
+    return tuple(args)
 
-#判断函数是否含有不可变关键字参数
 def has_named_kw_args(fn):
-	params = inspect.signature(fn).parameters
-	for name, param in params.items():
-		if param.kind == inspect.Parameter.KEYWORD_ONLY:
-			return True
+    params = inspect.signature(fn).parameters
+    for name, param in params.items():
+        if param.kind == inspect.Parameter.KEYWORD_ONLY:
+            return True
 
-#判断函数是否含有可变关键字参数
-def has_var_kw_args(fn):
-	params = inspect.signature(fn).parameters
-	for name, param in params.items():
-		if param.kind == inspect.Parameter.VAR_KEYWORD:
-			return True
+def has_var_kw_arg(fn):
+    params = inspect.signature(fn).parameters
+    for name, param in params.items():
+        if param.kind == inspect.Parameter.VAR_KEYWORD:
+            return True
 
-#判断函数是否有名为request的参数，如果有，是否为不可变关键字参数，
-#如果不是，则提示request参数比如为函数的最后一个参数
-def has_request_args(fn):
-	sig = inspect.signature(fn)
-	params = sig.parameters
-	found = False
-	for name, param in params.items():
-		if name == 'request':
-			found = True
-			continue
-		if found and (
-		param.kind != inspect.Parameter.VAR_POSITIONAL and
-		param.kind != inspect.Parameter.VAR_KEYWORD):
-			raise ValueError('request parameter must be the last named parameter in function: %s%s' % (fn, __name__, str(sig)))
-	return found
+def has_request_arg(fn):
+    sig = inspect.signature(fn)
+    params = sig.parameters
+    found = False
+    for name, param in params.items():
+        if name == 'request':
+            found = True
+            continue
+        if found and (param.kind != inspect.Parameter.VAR_POSITIONAL and param.kind != inspect.Parameter.KEYWORD_ONLY and param.kind != inspect.Parameter.VAR_KEYWORD):
+            raise ValueError('request parameter must be the last named parameter in function: %s%s' % (fn.__name__, str(sig)))
+    return found
 
-
-#处理request的函数，其实是转换url处理函数的一个函数
-#equestHandler目的就是从URL函数中分析其需要接收的参数，
-#从request中获取必要的参数，调用URL函数，然后把结果转换为web.Response对象
 class RequestHandler(object):
 
-	#构造函数初始化时调用
-	#RequestHandler在add_route的时候调用：
-	#app.router.add_route(method, path, RequestHandler(func))
-	def __init__(self, app, fn):
-		self._app = app
-		self._func = fn
-		self._has_request_arg = has_request_args(fn)
-		self._has_var_kw_arg = has_var_kw_args(fn)
-		self._has_named_kw_args = has_named_kw_args(fn)
-		self._named_kw_args = get_named_kw_args(fn)
-		self._required_kw_args = get_required_kw_args(fn)
+    def __init__(self, app, fn):
+        self._app = app
+        self._func = fn
+        self._has_request_arg = has_request_arg(fn)
+        self._has_var_kw_arg = has_var_kw_arg(fn)
+        self._has_named_kw_args = has_named_kw_args(fn)
+        self._named_kw_args = get_named_kw_args(fn)
+        self._required_kw_args = get_required_kw_args(fn)
 
-	async def find_post_kw(self, request, kw):
-
-		if not request.content_type:
-			return web.HttpBadReqeust('Missing Content-Type')
-		ct = request.content_type.lower()
-
-		if ct.startswith('applicatiion/json'):
-			params = await request.json()
-			if not isinstance(params, dict):
-				return web.HTTPBadRequest('JSON body must be object.')
-			kw = params
-
-		elif ct.startswith('application/x-www-form-urlencoded') or ct.startswith('multipart/form-data'):
-			params = await request.post()
-			kw = dict(**params)
-
-		else:
-			return web.HTTPBadRequest('Unsupportd Content-Type: %s')
-
-		return kw
-
-	def find_get_kw(self, request, kw):
-		qs = request.query_string
-		if qs:
-			kw = dict()
-			for k, v in parse.parse_qs(qs, True).items():
-				kw[k] = v[0]
-
-		return kw
-		
-	#定义该类的实例可以作为一个函数被调用，调用的函数参数，如__call__描述
-	#该函数在factorys的response_factory调用:
-	#r = await handler(request)
-	async def __call__(self, request):
-		kw = None
-		#检查POST参数和GET参数
-		#如果fn函数有可变/不可变关键字参数或有request参数
-		if self._has_var_kw_arg or self._has_named_kw_args or self._required_kw_args:
-			#如果是POST方法
-			if request.method == 'POST':
-				kw = self.find_post_kw(request, kw)
-			#如果是GET方法
-			if request.method == 'GET':
-				kw = self.find_get_kw(request, kw)
-
-		#如果参数为空，参数设为路由路径的参数，即request.match_info
-		if kw is None: #如果POST参数和GET参数为空
-			kw = dict(**request.match_info)
-		else: #如果POST参数和GET参数不为空
-			if not self._has_var_kw_arg and self._named_kw_args:
-				#remove all unamed kw:
-				copy = dict()
-				for name in self._named_kw_args:
-					if name in kw:
-						copy[name] = kw[name]
-				kw = copy
-
-			#check named arg:
-			for k, v in request.match_info.items():
-				if k in kw:
-					logging.warning('Duplicate arg name is named arg and kw args: %s' % k)
-				kw[k] = v
-
-		#如果有request参数，把request参数放进kw
-		if self._has_request_arg:
-			kw['request'] = request
-
-		#检查那些必不可少的参数:如果某个参数在kw中没有包含，则返回HttpBadReqeust提示缺少参数
-		if self._required_kw_args:
-			for name in self._required_kw_args:
-				if not name in kw:
-					return web.HttpBadRequest('Missing argument: %s' % name)
-
-
-		#打印日志，提示函数有哪些参数
-		logging.info('call with args: %s' % str(kw))
-
-
-		#调用fn函数，并将**kw参数传给fn函数处理，且得到返回的response
-		try:
-			r = await self._func(**kw)
-			return r
-		except APIError as e:
-			return dict(error=e.error, data=e.data, message=e.message)
-
+    @asyncio.coroutine
+    def __call__(self, request):
+        kw = None
+        if self._has_var_kw_arg or self._has_named_kw_args or self._required_kw_args:
+            if request.method == 'POST':
+                if not request.content_type:
+                    return web.HTTPBadRequest('Missing Content-Type.')
+                ct = request.content_type.lower()
+                if ct.startswith('application/json'):
+                    params = yield from request.json()
+                    if not isinstance(params, dict):
+                        return web.HTTPBadRequest('JSON body must be object.')
+                    kw = params
+                elif ct.startswith('application/x-www-form-urlencoded') or ct.startswith('multipart/form-data'):
+                    params = yield from request.post()
+                    kw = dict(**params)
+                else:
+                    return web.HTTPBadRequest('Unsupported Content-Type: %s' % request.content_type)
+            if request.method == 'GET':
+                qs = request.query_string
+                if qs:
+                    kw = dict()
+                    for k, v in parse.parse_qs(qs, True).items():
+                        kw[k] = v[0]
+        if kw is None:
+            kw = dict(**request.match_info)
+        else:
+            if not self._has_var_kw_arg and self._named_kw_args:
+                # remove all unamed kw:
+                copy = dict()
+                for name in self._named_kw_args:
+                    if name in kw:
+                        copy[name] = kw[name]
+                kw = copy
+            # check named arg:
+            for k, v in request.match_info.items():
+                if k in kw:
+                    logging.warning('Duplicate arg name in named arg and kw args: %s' % k)
+                kw[k] = v
+        if self._has_request_arg:
+            kw['request'] = request
+        # check required kw:
+        if self._required_kw_args:
+            for name in self._required_kw_args:
+                if not name in kw:
+                    return web.HTTPBadRequest('Missing argument: %s' % name)
+        logging.info('call with args: %s' % str(kw))
+        try:
+            r = yield from self._func(**kw)
+            return r
+        except APIError as e:
+            return dict(error=e.error, data=e.data, message=e.message)
 
 def add_static(app):
-	path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
-	app.router.add_static('/static/', path)
-	logging.info('add static %s ==> %s' % ('/static/', path))
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+    app.router.add_static('/static/', path)
+    logging.info('add static %s => %s' % ('/static/', path))
 
-
-#编写一个add_route函数，用来注册一个URL处理函数
-#从url函数中获取方法和路径信息，并检查协程，然后：
-#设置路由：app.router.add_route(method, path, handlerFunc)
 def add_route(app, fn):
-	#获取get/post函数中的method和route参数
-	method = getattr(fn, '__method__', None)
-	path = getattr(fn, '__route__', None)
+    method = getattr(fn, '__method__', None)
+    path = getattr(fn, '__route__', None)
+    if path is None or method is None:
+        raise ValueError('@get or @post not defined in %s.' % str(fn))
+    if not asyncio.iscoroutinefunction(fn) and not inspect.isgeneratorfunction(fn):
+        fn = asyncio.coroutine(fn)
+    logging.info('add route %s %s => %s(%s)' % (method, path, fn.__name__, ', '.join(inspect.signature(fn).parameters.keys())))
+    app.router.add_route(method, path, RequestHandler(app, fn))
 
-	#如果路径或者方法名称为空，返回错误提示
-	if path is None or method is None:
-		raise ValueError('@get or @post not defined in %s：' % str(fn))
-
-	#如果不是协程函数，且不是装饰函数，将其加入协程
-	if not asyncio.iscoroutinefunction(fn) and not inspect.isgeneratorfunction(fn):
-		fn = asyncio.coroutine(fn)
-
-	logging.info('add route %s %s => %s(%s)' % (method, path, fn.__name__, ' , '.join(inspect.signature(fn).parameters.keys())))
-
-	#将url处理函数通过RequestHandler处理后，返回RequestHandler的实例handler
-	#即以handler作为函数对象传递给add_route()的第三个参数
-	app.router.add_route(method, path, RequestHandler(app, fn))
-
-
-#把很多次add_route()注册的调用：
-#add_route(app, handles.index)
-#add_route(app, handles.blog)
-#add_route(app, handles.create_comment)
-#变为自动扫描：add_routes(app, 'handlers')
-#即自动把handler模块的所有符合条件的函数注册了
 def add_routes(app, module_name):
-	n = module_name.rfind('.')
-	if n == (-1):
-		mod = __import__(module_name, globals(), locals())
-	else:
-		name =  module_name[n+1:]
-		mod = getattr(__import__(module_name[:n], globals(), locals(), [name]), name)
-
-	for attr in dir(mod):
-		if attr.startswith('_'):
-			continue
-		fn = getattr(mod, attr)
-		if callable(fn):
-			method = getattr(fn, '__method__', None)
-			path = getattr(fn, '__route__', None)
-			if method and path:
-				add_route(app, fn)
+    n = module_name.rfind('.')
+    if n == (-1):
+        mod = __import__(module_name, globals(), locals())
+    else:
+        name = module_name[n+1:]
+        mod = getattr(__import__(module_name[:n], globals(), locals(), [name]), name)
+    for attr in dir(mod):
+        if attr.startswith('_'):
+            continue
+        fn = getattr(mod, attr)
+        if callable(fn):
+            method = getattr(fn, '__method__', None)
+            path = getattr(fn, '__route__', None)
+            if method and path:
+                add_route(app, fn)
--- www/handlers.py
+++ www/handlers.py
@@ -1,95 +1,126 @@
 #!/usr/bin/env python3
-#-*- coding:utf-8 -*-
+# -*- coding: utf-8 -*-
 
-__author__ = 'sanjinmr'
+__author__ = 'Michael Liao'
 
-'url handlers'
+' url handlers '
 
-from coroweb import get
+import re, time, json, logging, hashlib, base64, asyncio
 
-from models import User, Blog, next_id
+from aiohttp import web
 
-import time
+from coroweb import get, post
+from apis import APIValueError, APIResourceNotFoundError
 
-from apis import Page, APIValueError
-
+from models import User, Comment, Blog, next_id
+from config import configs
 
 COOKIE_NAME = 'awesession'
 _COOKIE_KEY = configs.session.secret
-
-def get_page_index(page_str):
-	p = 1
-	try:
-		p = int(page_str)
-	except ValueError as e:
-		pass
-	if p < 1:
-		p = 1
-	return p
-	
-def user2cookie(uesr, max_age):
-	'''
-	Generate cookie str by user.
-	'''
-	#build cookie string by: id-express-sha1
-	expires = str(int(time.time() + max_age))
-	s = '%s-%s-%s-%s' % (user.id, user.passwd, expires, _COOKIE_KEY)
-	L = [user.id, expires, hashlib.sha1(s.encode('utf-8')).hexdigest()]
-	return '-'.join(L)
-	
-	
-@get('/')
-async def index(request):
-	users = await User.findAll()
-	summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-	blogs = [
-		Blog(id='1', name='Test Blog 1', summary=summary, created_at=time.time()-120),
-		Blog(id='2', name='Test Blog 2', summary=summary, created_at=time.time()-3600),
-		Blog(id='3', name='Test Blog 3', summary=summary, created_at=time.time()-7200)
-	]
-	return {
-		'__template__': 'blogs.html',
-		'users': users,
-		'blogs': blogs
-	}
-
-@get('/api/users')
-async def api_get_users(*, page = '1'):
-	page_index = get_page_index(page)
-	num = await User.findNumber('count(id)')
-	p = Page(num, page_index)
-	if num == 0:
-		return dict(page = p, users = ())
-	users = await User.findAll(orderBy = 'created_at desc', limit = (p.offset, p.limit))
-	for u in users:
-		u.passwd = '******'
-	return dict(page = p, users = users)
-	
 _RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
 _RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
 
-@POST('/api/users')
+def user2cookie(user, max_age):
+    '''
+    Generate cookie str by user.
+    '''
+    # build cookie string by: id-expires-sha1
+    expires = str(int(time.time() + max_age))
+    s = '%s-%s-%s-%s' % (user.id, user.passwd, expires, _COOKIE_KEY)
+    L = [user.id, expires, hashlib.sha1(s.encode('utf-8')).hexdigest()]
+    return '-'.join(L)
+
+@asyncio.coroutine
+def cookie2user(cookie_str):
+    '''
+    Parse cookie and load user if cookie is valid.
+    '''
+    if not cookie_str:
+        return None
+    try:
+        L = cookie_str.split('-')
+        if len(L) != 3:
+            return None
+        uid, expires, sha1 = L
+        if int(expires) < time.time():
+            return None
+        user = yield from User.find(uid)
+        if user is None:
+            return None
+        s = '%s-%s-%s-%s' % (uid, user.passwd, expires, _COOKIE_KEY)
+        if sha1 != hashlib.sha1(s.encode('utf-8')).hexdigest():
+            logging.info('invalid sha1')
+            return None
+        user.passwd = '******'
+        return user
+    except Exception as e:
+        logging.exception(e)
+        return None
+
+@get('/')
+def index(request):
+    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    blogs = [
+        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+        Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
+        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
+    ]
+    return {
+        '__template__': 'blogs.html',
+        'blogs': blogs
+    }
+
+@get('/register')
+def register():
+    return {
+        '__template__': 'register.html'
+    }
+
+@post('/api/authenticate')
+def authenticate(*, email, passwd):
+    if not email:
+        raise APIValueError('email', 'Invalid email.')
+    if not passwd:
+        raise APIValueError('passwd', 'Invalid password.')
+    users = yield from User.findAll('email=?', [email])
+    if len(users) == 0:
+        raise APIValueError('email', 'Email not exist.')
+    user = users[0]
+    # check passwd:
+    sha1 = hashlib.sha1()
+    sha1.update(user.id.encode('utf-8'))
+    sha1.update(b':')
+    sha1.update(passwd.encode('utf-8'))
+    if user.passwd != sha1.hexdigest():
+        raise APIValueError('passwd', 'Invalid password.')
+    # authenticate ok, set cookie:
+    r = web.Response()
+    r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
+    user.passwd = '******'
+    r.content_type = 'application/json'
+    r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
+    return r
+
+@post('/api/users')
+@asyncio.coroutine
 def api_register_user(*, email, name, passwd):
-	if not name or not name.strip():
-		raise APIValueError('name')
-	if not email or not _RE_EMAIL.match(email):
-		raise APIValueError('email')
-	if not passwd or not _RE_SHA1.match(passwd):
-		raise APIValueError('passwd')
-	users = await User.findAll('email=?', [email])
-	if len(users) > 0:
-		raise APIError('register:failed', 'email', 'Email is already in use.')
-	uid = next_id()
-	sha1_passwd = '%s:%s' % (uid, passwd)
-	user = User(id=uid, name=name.strip(), email=email)
-	await user.save()
-	# make session cookie
-	r = web.Response()
-	r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
-	user.passwd = '******'
-	r.content_type = 'application/json'
-	r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
-	return r
-	
-	
-	
+    if not name or not name.strip():
+        raise APIValueError('name')
+    if not email or not _RE_EMAIL.match(email):
+        raise APIValueError('email')
+    if not passwd or not _RE_SHA1.match(passwd):
+        raise APIValueError('passwd')
+    users = yield from User.findAll('email=?', [email])
+    if len(users) > 0:
+        raise APIError('register:failed', 'email', 'Email is already in use.')
+    uid = next_id()
+    sha1_passwd = '%s:%s' % (uid, passwd)
+    user = User(id=uid, name=name.strip(), email=email, passwd=hashlib.sha1(sha1_passwd.encode('utf-8')).hexdigest(), image='http://www.gravatar.com/avatar/%s?d=mm&s=120' % hashlib.md5(email.encode('utf-8')).hexdigest())
+    yield from user.save()
+    # make session cookie:
+    r = web.Response()
+    r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
+    user.passwd = '******'
+    r.content_type = 'application/json'
+    r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
+    return r
